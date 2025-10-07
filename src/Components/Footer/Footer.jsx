4eb5a2f170e8159346417e0911e08f41bb6322b5
WaliullahKawsar2 <waliullahkawsar8@gmail.com>
2025-10-08 00:26:23 +0600

    footer design

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import logo from "../../assets/logo.png"
+import { FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
-    return (
-        <div>
-            footer
+  return (
+    <footer className="footer footer-horizontal footer-center  bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent ">
+      <aside>
+        <img className="w-20" src={logo} alt="" />
+        <p className="font-bold">
+          HERO.IO
+          <br />
+          Providing reliable tech since 1992
+        </p>
+        <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+      </aside>
+      <nav>
+        <div className="grid grid-flow-col gap-4">
+          <FaTwitter className="text-[#9F62F2] cursor-pointer" />
+          <FaYoutube className="text-[#9F62F2] cursor-pointer" />
+          <FaFacebook className="text-[#9F62F2] cursor-pointer" />
+          
         </div>
-    );
+      </nav>
+    </footer>
+  );
 };
 
 export default Footer;
