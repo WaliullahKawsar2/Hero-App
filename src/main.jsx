bf1249a58b578c146be28a0be20464b58e867e77
WaliullahKawsar2 <waliullahkawsar8@gmail.com>
2025-10-08 00:14:06 +0600

    navbar design

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import MainLayout from './Layouts/MainLayout.jsx'
+import Home from './Pages/Home.jsx'
+import Apps from './Pages/Apps.jsx'
+import Installation from './Pages/Installation.jsx'
+
+const router = createBrowserRouter([{
+  path:'/',
+  Component:MainLayout,
+  children:[
+    {
+      path:'/',
+      Component:Home
+    },
+    {
+      path:'/apps',
+      Component:Apps
+    },
+    {
+      path:'/installation',
+      Component:Installation
+    }
+  ]
+}])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
